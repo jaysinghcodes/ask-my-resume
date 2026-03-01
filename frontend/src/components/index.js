2198fb2eb1d54e21d0496ccf35acc536c6c3fb71
Jay Singh <jaggys44@gmail.com>
2026-02-28 18:27:52 -0600

    reformatted structure

---
+++ frontend/src/components/index.js
@@ -0,0 +1,8 @@
+export { TypingIndicator } from "./TypingIndicator";
+export { Message } from "./Message";
+export { MessageAvatar } from "./MessageAvatar";
+export { EmptyState } from "./EmptyState";
+export { ChatInput } from "./ChatInput";
+export { ChatHeader } from "./ChatHeader";
+export { MessagesList } from "./MessagesList";
+export { NavDrawer } from "./NavDrawer";
